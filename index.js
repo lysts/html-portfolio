43a57b1fbb9f5a5c648e7de0631ea897aeef8efa
Lyla <123667606+lysts@users.noreply.github.com>
2024-01-10 17:13:36 +0000

    new edits

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 humanAge = prompt("How long have you resided on this planet for?");
 dogAge = ((humanAge - 21)/4)+2;
 alert("If you were a dog, you would be " + dogAge + " years old!");
+
+
+usersname = prompt("What is your name?");
+userscrush = prompt("Who is the lucky crush?");
+var lovescore = Math.random();
+lovescore = Math.floor(lovescore*100);
+if (lovescore > 70) {
+    alert(usersname + " and " + userscrush + " are totes in love! Their lovescore is " + lovescore + "%!");
+} else {
+    alert(usersname + " and " + userscrush + " met and their lovescore is " + lovescore + "%.");
+}
+
+var output = []
+
+function fibonacciGenerator (n) {
+
+    if (n === 1) {
+        output = [0]
+    
+    } else if (n === 2) {
+        output = [0, 1]
+
+    } else {
+        fibonacciGenerator(n-1)
+        var newFib = output[n-2] + output[n-3]
+        output.push(newFib)
+    }
+
+    return output
+}
